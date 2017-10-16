7bf1346edfbf9432b77678cbdce2dc02d47794f1
Yuliya Yasenetska <jasenetska@gmail.com>
2017-10-16 20:17:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,19 +1,15 @@
-#languages = {
-#  :oo => {
-#    :ruby => {
-#      :type => "interpreted"
-#    }
-#CHANGE TO:
-#new_hash= {
-#  :ruby => {
-#    :type => "interpreted",
-#    :style => [:oo]
-#  }
 
 def reformat_languages(languages)
  new_hash={}
- languages.each do |key, value|
-   new_hash<<value
-    new_hash[:key]={style:"#{languages[0]}"}
- end
+  languages.each do |key, hash|
+    hash.each do |name, type|
+      new_hash[name] ||= type
+      new_hash[name][:style] ||= []
+      new_hash[name][:style]<<key
+    end
+  end
+  new_hash
 end
+
+
+# ||= assignment operator -  if it's non-nil or true
