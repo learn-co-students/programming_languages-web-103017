3b36b3988262c5023358594eadfba8c9e38cf823
Yuliya Yasenetska <jasenetska@gmail.com>
2017-10-16 20:02:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+#languages = {
+#  :oo => {
+#    :ruby => {
+#      :type => "interpreted"
+#    }
+#CHANGE TO:
+#new_hash= {
+#  :ruby => {
+#    :type => "interpreted",
+#    :style => [:oo]
+#  }
+
 def reformat_languages(languages)
-  # your code here
+ new_hash={}
+ languages.each do |key, value|
+   new_hash<<value
+    new_hash[:key]={style:"#{languages[0]}"}
+ end
 end
