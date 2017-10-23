0790737f94e6ed2287f2abdcd648962b8fd9e9a1
Erick Camacho <erickcamacho1@gmail.com>
2017-10-23 16:49:48 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+    new_hash = {}
+    languages.each do |style, language|
+        language.each do |lan_name, lan_data|
+            new_hash[lan_name] = lan_data
+            new_hash[lan_name][:style] = []
+            new_hash[lan_name][:style].push(style)
+        end
+    end
+
+    new_hash[:javascript][:style].push(:oo) #takes care of adding "oo" to javascript which already has a functional stylr
+    new_hash
+
 end
